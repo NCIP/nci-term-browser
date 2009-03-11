277ecf2b665f0a8089058402759ed809601dfcdb
Will Garcia <garciaw>
2009-03-11 13:23:18 +0000

    Performance tests
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/trunk/software@84 0604bb77-1110-461e-859e-28c50bf9b280

---
+++ test/performance/src/gov/nih/nci/evs/browser/utils/Util.java
@@ -0,0 +1,36 @@
+package gov.nih.nci.evs.browser.utils;
+
+import java.text.DecimalFormat;
+
+public class Util {
+	public static final String SEPARATOR = 
+		"----------------------------------------" +
+		"----------------------------------------";
+    private static DecimalFormat _doubleFormatter = new DecimalFormat("0.00");
+
+    public static class StopWatch {
+        private long _startMS = 0;
+        
+        public StopWatch() {
+            start();
+        }
+    
+        public void start() {
+            _startMS = System.currentTimeMillis();
+        }
+        
+        public long duration() {
+            return System.currentTimeMillis() - _startMS;
+        }
+        
+        public String getResult() {
+            long time = duration();
+            double timeSec = time/1000.0;
+            double timeMin = timeSec/60.0;
+            
+            return "Run time: " + time + " ms, " + 
+                _doubleFormatter.format(timeSec) + " sec, " + 
+                _doubleFormatter.format(timeMin) + " min";
+        }
+    }
+}
