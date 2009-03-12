941b0f8a20de300512d6cb800f6185dfdf277d29
Will Garcia <garciaw>
2009-03-12 19:52:35 +0000

    Removed "<script language="JavaScript">" to clean script within a script error in IE.
    SVN-Revision: 92

--- software/ncitbrowser/web/js/search.js
+++ software/ncitbrowser/web/js/search.js
@@ -1,22 +1,19 @@
-<script language="JavaScript">
-function changeMenuStyle(obj, new_style) { 
-  obj.className = new_style; 
+function changeMenuStyle(obj,new_style) {
+  obj.className = new_style
 }
 
-function showCursor(){
-	document.body.style.cursor='hand'
+function showCursor() {
+    document.body.style.cursor = "hand"
 }
 
-function hideCursor(){
-	document.body.style.cursor='default'
+function hideCursor() {
+    document.body.style.cursor = "default"
 }
 
-function confirmDelete(){
-  if (confirm('Are you sure you want to delete?')){
-    return true;
-    }else{
-    return false;
-  }
+function confirmDelete() {
+    if (confirm("Are you sure you want to delete?"))
+        return true
+    return false
 }
 
 function setUrl ( url, target ) {
@@ -29,61 +26,55 @@ function setUrl ( url, target ) {
 
 function spawn(url,winw,winh) {
   var w = window.open(url, "_blank",
-      "screenX=0,screenY=0,status=yes,toolbar=yes,menubar=yes,location=yes,width=" + winw + ",height=" + winh + 
+      "screenX=0,screenY=0,status=yes,toolbar=yes,menubar=yes,location=yes,width=" + winw + ",height=" + winh +
       ",scrollbars=yes,resizable=yes");
-} 
+}
 
 function spawnX(url,winw,winh) {
   var w = window.open(url, "_blank",
-      "screenX=0,screenY=0,status=no,toolbar=yes,menubar=no,location=no,width=" + winw + ",height=" + winh + 
+      "screenX=0,screenY=0,status=no,toolbar=yes,menubar=no,location=no,width=" + winw + ",height=" + winh +
       ",scrollbars=yes,resizable=yes");
-} 
+}
 
-function showWindow(imgscr){
-                myWind = window.open(imgscr,"subWindow","HEIGHT=520,WIDTH=520,resizable");
-                myWind.focus();
-        }
+function showWindow(imgscr) {
+       myWind = window.open(imgscr,"subWindow","HEIGHT=520,WIDTH=520,resizable");
+       myWind.focus()
+}
 
 function URLencode(sStr) {
-    return escape(sStr).replace(/\+/g, '%2C').replace(/\"/g,'%22').replace(/\'/g, '%27');
-  }
+    return escape(sStr).replace(/\+/g, '%2C').replace(/\"/g,'%22').replace(/\'/g, '%27')
+}
 
-
- // ---------- Begin URL encode javascript function from CMAP ------------------
+// *********************** Begin URL encode javascript function from CMAP **********************
 
 var active = 'false';
 var NS6 = (document.getElementById && !document.all) ? true : false;
 
-function ifenter(e,form)
-{
+function ifenter(e,form) {
   if (!active) return;
 
   var keycode;
   if (window.event)         // IE
     keycode = window.event.keyCode;
   else
-  if (e) {                  // NS 
+  if (e) {                  // NS
     if (NS6)
       keycode = e.keyCode;  // NS6: this shows bksp, enter ...
     else
-      keycode = e.which;    // NS6: real   NS4: all keys  
+      keycode = e.which;    // NS6: real   NS4: all keys
   }
   else return true;
 
-  if (NS6)
-  {
-    if (keycode == 13)
-    {      
+  if (NS6) {
+    if (keycode == 13) {
       Search(form);
       e.cancelBubble = true;
       return false;
-    } 
+    }
     else
       return true;
   }
-  else
-  if (keycode == 13)
-  {
+  else if (keycode == 13) {
     Search(form);
     return false;
   }
@@ -91,31 +82,21 @@ function ifenter(e,form)
     return true;
 }
 
+function Search(form) {
 
-
-  function Search(form)
-  {
     var searchTerm1 = document.SiteSearch.term.value;
     var searchTerm = searchTerm1.replace(/ /g, "%20");
-    
-    if ( ( searchTerm == null ) || ( searchTerm == "" ) || ( searchTerm == " ") )
-    {
+
+    if ( ( searchTerm == null ) || ( searchTerm == "" ) || ( searchTerm == " ") ) {
         alert("Please enter a search term.");
-    }
-    else
-    {
+    } else {
         location = '/NCICB/ZopeSearch?start:int=0&term=' + searchTerm;
     }
- }
- 
 
-function setSize()
-{
- if(!document.layers)
-  {
+}
+
+function setSize() {
+ if(!document.layers) {
    document.forms[0].term.size=15;
   }
 }
-
-
-</script>
