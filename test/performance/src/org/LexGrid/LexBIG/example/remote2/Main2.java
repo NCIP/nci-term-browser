a979a395e19a5ce014fbbab48d9909da16989a4d
David Yee <yeed>
2009-03-24 16:21:29 +0000

    GF19233: Main2 can now call ConceptHistory test program.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/trunk/software@155 0604bb77-1110-461e-859e-28c50bf9b280

--- test/performance/src/org/LexGrid/LexBIG/example/remote2/Main2.java
+++ test/performance/src/org/LexGrid/LexBIG/example/remote2/Main2.java
@@ -2,6 +2,7 @@ package org.LexGrid.LexBIG.example.remote2;
 
 import gov.nih.nci.evs.browser.utils.SearchUtils;
 
+import org.LexGrid.LexBIG.Impl.test.ConceptHistory;
 import org.LexGrid.LexBIG.util.Constants;
 import org.LexGrid.LexBIG.util.Prompt;
 
@@ -21,6 +22,7 @@ public class Main2 implements Constants {
             println(INDENT + "4) " + "MetaMatch2");
             println(INDENT + "5) " + "SearchUtils") ;
             println(INDENT + "6) " + "BuildTreeForCode2") ;
+            println(INDENT + "7) " + "ConceptHistory") ;
             println(INDENT + "0) " + "Quit");
             println(SEPARATOR2);
             i = Prompt.prompt("Choose", i);
@@ -34,6 +36,7 @@ public class Main2 implements Constants {
                 case 4: new MetaMatch2().run(); break;
                 case 5: SearchUtils.main(new String[] { null }); break;
                 case 6: BuildTreeForCode2.run(); break;
+                case 7: new ConceptHistory().run(); break;
                 default: println("Invalid choice.  Try again."); break;
                 }
                 println("");
