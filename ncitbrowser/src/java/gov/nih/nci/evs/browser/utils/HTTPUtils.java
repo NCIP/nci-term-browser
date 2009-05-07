315b06eb7c890ea263c56856cb21bc6e77ea0cb1
Will Garcia <garciaw>
2009-05-07 19:03:03 +0000

    Made removal of '<script>' XSS attacks case insensitive.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/trunk/software@503 0604bb77-1110-461e-859e-28c50bf9b280

--- ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/HTTPUtils.java
+++ ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/HTTPUtils.java
@@ -2,6 +2,7 @@ package gov.nih.nci.evs.browser.utils;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.regex.Pattern;
 
 /**
  * HTTP Utility methods
@@ -19,7 +20,7 @@ public class HTTPUtils {
 
 		if (value == null || value.length() < 1)
 			return value;
-
+		
 		try {
 			value = URLDecoder.decode(value, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
@@ -27,16 +28,29 @@ public class HTTPUtils {
 		}
 
 		// Remove XSS attacks
+		value = replaceAll(value,"<\\s*script\\s*>.*</\\s*script\\s*>", "");		
 		value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 		value = value.replaceAll("\\(", "&#40;").replaceAll("\\)", "&#41;");
 		value = value.replaceAll("'", "&#39;");
 		value = value.replaceAll("eval\\((.*)\\)", "");
-		value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']",
-				"\"\"");
-		value = value.replaceAll("script", "");
+		value = replaceAll(value,"[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");		
 		value = value.replaceAll("\"", "&quot;");
 		return value;
 
 	}
     
+	/**
+	 * @param string
+	 * @param regex
+	 * @param replaceWith
+	 * @return
+	 */
+	public static String replaceAll(String string, String regex, String replaceWith) {
+		
+		Pattern myPattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		string = myPattern.matcher(string).replaceAll(replaceWith);
+		return string;
+		
+	} 
+	
 }
