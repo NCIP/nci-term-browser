0dd25a8335cd8436be6b463837314c3d60f35b27
Will Garcia <garciaw>
2009-05-07 16:40:35 +0000

    Added - replace " with &quot;
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/trunk/software@500 0604bb77-1110-461e-859e-28c50bf9b280

--- ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/HTTPUtils.java
+++ ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/HTTPUtils.java
@@ -1,5 +1,8 @@
 package gov.nih.nci.evs.browser.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 /**
  * HTTP Utility methods
  * @author garciawa2
@@ -8,21 +11,32 @@ package gov.nih.nci.evs.browser.utils;
 public class HTTPUtils {
 
     /**
-     * Remove potentially bad XSS syntax 
-     * @param value
-     * @return
-     */
-    public static String cleanXSS(String value) {
-    	
-      if (value == null || value.length() < 1) return value; 
-      value = value.replaceAll("<", "& lt;").replaceAll(">", "& gt;");
-      value = value.replaceAll("\\(", "& #40;").replaceAll("\\)", "& #41;");
-      value = value.replaceAll("'", "& #39;");
-      value = value.replaceAll("eval\\((.*)\\)", "");
-      value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']","\"\"");
-      value = value.replaceAll("script", "");
-      return value;
-      
-    }
+	 * Remove potentially bad XSS syntax 
+	 * @param value
+	 * @return
+	 */
+	public static String cleanXSS(String value) {
+
+		if (value == null || value.length() < 1)
+			return value;
+
+		try {
+			value = URLDecoder.decode(value, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// Do nothing, just use the input
+		}
+
+		// Remove XSS attacks
+		value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+		value = value.replaceAll("\\(", "&#40;").replaceAll("\\)", "&#41;");
+		value = value.replaceAll("'", "&#39;");
+		value = value.replaceAll("eval\\((.*)\\)", "");
+		value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']",
+				"\"\"");
+		value = value.replaceAll("script", "");
+		value = value.replaceAll("\"", "&quot;");
+		return value;
+
+	}
     
 }
