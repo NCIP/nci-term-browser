f77b775f501ce05421ef1bd80d323e605428c414
Will Garcia <garciaw>
2009-05-08 14:42:15 +0000

    Code cleanup. Formatting and added CODING_SCHEME_NAME as a static constant.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/trunk/software@510 0604bb77-1110-461e-859e-28c50bf9b280

--- ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/HistoryUtils.java
+++ ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/HistoryUtils.java
@@ -16,64 +16,66 @@ import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
 import org.LexGrid.LexBIG.Utility.Constructors;
 import org.LexGrid.concepts.Concept;
 
+import static gov.nih.nci.evs.browser.common.Constants.*;
+
 public class HistoryUtils {
-    private static final String CODING_SCHEME = "NCI Thesaurus";
-    private static DateFormat _dataFormatter = new SimpleDateFormat("yyyy-MM-dd");
+	
+	private static DateFormat _dataFormatter = new SimpleDateFormat(
+			"yyyy-MM-dd");
 
-    public static Vector<String> getTableHeader() {
-        Vector<String> v = new Vector<String>();
-        v.add("Edit Actions");
-        v.add("Date");
-        v.add("Reference Concept");
-        return v;
-    }
+	public static Vector<String> getTableHeader() {
+		Vector<String> v = new Vector<String>();
+		v.add("Edit Actions");
+		v.add("Date");
+		v.add("Reference Concept");
+		return v;
+	}
 
-    public static Vector<String> getEditActions(String codingSchemeName,
-            String vers, String ltag, String code) throws LBException {
-        LexBIGService lbSvc = RemoteServerUtil.createLexBIGService();
-        HistoryService hs = lbSvc.getHistoryService(CODING_SCHEME);
+	public static Vector<String> getEditActions(String codingSchemeName,
+			String vers, String ltag, String code) throws LBException {
+		LexBIGService lbSvc = RemoteServerUtil.createLexBIGService();
+		HistoryService hs = lbSvc.getHistoryService(CODING_SCHEME_NAME);
 
-        try {
+		try {
 			NCIChangeEventList list = hs.getEditActionList(Constructors
-				.createConceptReference(code, null), null, null);
+					.createConceptReference(code, null), null, null);
 			return getEditActions(codingSchemeName, vers, ltag, code, list);
-	    } catch (Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return null;
-    }
+	}
 
-    private static Vector<String> getEditActions(String codingSchemeName,
-            String vers, String ltag, String code, NCIChangeEventList list) {
-        Enumeration<NCIChangeEvent> enumeration = list.enumerateEntry();
-        Vector<String> v = new Vector<String>();
-        HashSet<String> hset = new HashSet<String>();
-        while (enumeration.hasMoreElements()) {
-            NCIChangeEvent event = enumeration.nextElement();
-            ChangeType type = event.getEditaction();
-            Date date = event.getEditDate();
-            String rCode = event.getReferencecode();
-            String desc = "N/A";
-            if (rCode != null && rCode.length() > 0 &&
-                    ! rCode.equalsIgnoreCase("null")) {
-                Concept c = DataUtils.getConceptByCode(
-                    codingSchemeName, vers, ltag, rCode);
-                //KLO
-                if (c != null) {
+	private static Vector<String> getEditActions(String codingSchemeName,
+			String vers, String ltag, String code, NCIChangeEventList list) {
+		Enumeration<NCIChangeEvent> enumeration = list.enumerateEntry();
+		Vector<String> v = new Vector<String>();
+		HashSet<String> hset = new HashSet<String>();
+		while (enumeration.hasMoreElements()) {
+			NCIChangeEvent event = enumeration.nextElement();
+			ChangeType type = event.getEditaction();
+			Date date = event.getEditDate();
+			String rCode = event.getReferencecode();
+			String desc = "N/A";
+			if (rCode != null && rCode.length() > 0
+					&& !rCode.equalsIgnoreCase("null")) {
+				Concept c = DataUtils.getConceptByCode(codingSchemeName, vers,
+						ltag, rCode);
+				//KLO
+				if (c != null) {
 					String name = c.getEntityDescription().getContent();
 					desc = name + " (Code " + rCode + ")";
-			    } else {
+				} else {
 					desc = rCode;
 				}
-            }
+			}
 
-            String info = type + "|" + _dataFormatter.format(date) + "|" + desc;
-            if (hset.contains(info))
-                continue;
-            //System.out.println("NCIChangeEvent: " + info);
-            v.add(info);
-            hset.add(info);
-        }
-        return v;
-    }
+			String info = type + "|" + _dataFormatter.format(date) + "|" + desc;
+			if (hset.contains(info))
+				continue;
+			v.add(info);
+			hset.add(info);
+		}
+		return v;
+	}
 }
