01af07318287d90ccdd63341838430065ec2ec64
Will Garcia <garciaw>
2009-06-02 16:57:59 +0000

    Addition of clean command. Really cleans by removing \target.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/trunk@582 0604bb77-1110-461e-859e-28c50bf9b280

--- software/build/build.cmd
+++ software/build/build.cmd
@@ -16,9 +16,17 @@ if "%1" == "deploy" (
     ant deploy:hot
     goto DONE
 )
+if "%1" == "clean" (
+    ant clean
+    if exist ..\target\*.* (
+       rmdir /Q /S ..\target
+    )
+    goto DONE
+)
 if "%1" == "dev" (
     ant -Dproperties.file=C:\SVN-Projects\ncit-properties\properties\dev-upgrade.properties deploy:remote:upgrade
     goto DONE
 )
+
 :DONE
 endlocal
