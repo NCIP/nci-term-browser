4bb100ee2e8b4ab331b08fe2955d8b6dd5ab7d5a
David Yee <yeed>
2010-06-09 12:17:35 +0000

    Converted System.out.println statements to log4j in properties package.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/trunk@1369 0604bb77-1110-461e-859e-28c50bf9b280

--- software/ncitbrowser/src/java/gov/nih/nci/evs/browser/properties/PropertyFileParser.java
+++ software/ncitbrowser/src/java/gov/nih/nci/evs/browser/properties/PropertyFileParser.java
@@ -11,6 +11,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -55,7 +56,7 @@ import gov.nih.nci.evs.browser.bean.SecurityTokenHolder;
  */
 
 public class PropertyFileParser {
-
+    private static Logger _logger = Logger.getLogger(PropertyFileParser.class);
 	List displayItemList;
 	HashMap configurableItemMap;
 
@@ -262,7 +263,7 @@ public class PropertyFileParser {
 	private void printData(){
 		Iterator it = displayItemList.iterator();
 		while(it.hasNext()) {
-			System.out.println(it.next().toString());
+			_logger.debug(it.next().toString());
 		}
 	}
 
