6627f72b428e6f57303de3c411c2284c4a9c0ce2
David Yee <yeed>
2010-06-09 17:01:56 +0000

    Reformatted copyright notice in the all the packages.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/trunk@1374 0604bb77-1110-461e-859e-28c50bf9b280

--- software/ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/TreeUtils.java
+++ software/ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/TreeUtils.java
@@ -1,35 +1,5 @@
 package gov.nih.nci.evs.browser.utils;
 
-/*
- * Copyright: (c) 2004-2009 Mayo Foundation for Medical Education and
- * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
- * triple-shield Mayo logo are trademarks and service marks of MFMER.
- *
- * Except as contained in the copyright notice above, or as used to identify
- * MFMER as the author of this software, the trade names, trademarks, service
- * marks, or product names of the copyright holder shall not be used in
- * advertising, promotion or otherwise in connection with this software without
- * prior written authorization of the copyright holder.
- *
- * Licensed under the Eclipse Public License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.eclipse.org/legal/epl-v10.html
- *
- */
-
-/**
- * @author EVS Team
- * @version 1.0
- *
- * Note: This class is created based on Mayo's BuildTreForCode.java sample code
- *
- * Modification history
- *     Initial modification kim.ong@ngc.com
- *
- */
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -74,6 +44,78 @@ import org.LexGrid.LexBIG.Utility.ConvenienceMethods;
 import org.apache.log4j.Logger;
 
 //import static gov.nih.nci.evs.browser.common.Constants.*;
+
+/**
+ * Copyright: (c) 2004-2009 Mayo Foundation for Medical Education and
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+
+/**
+ * <!-- LICENSE_TEXT_START -->
+ * Copyright 2008,2009 NGIT. This software was developed in conjunction 
+ * with the National Cancer Institute, and so to the extent government 
+ * employees are co-authors, any rights in such works shall be subject 
+ * to Title 17 of the United States Code, section 105.
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ *   1. Redistributions of source code must retain the above copyright 
+ *      notice, this list of conditions and the disclaimer of Article 3, 
+ *      below. Redistributions in binary form must reproduce the above 
+ *      copyright notice, this list of conditions and the following 
+ *      disclaimer in the documentation and/or other materials provided 
+ *      with the distribution.
+ *   2. The end-user documentation included with the redistribution, 
+ *      if any, must include the following acknowledgment:
+ *      "This product includes software developed by NGIT and the National 
+ *      Cancer Institute."   If no such end-user documentation is to be
+ *      included, this acknowledgment shall appear in the software itself,
+ *      wherever such third-party acknowledgments normally appear.
+ *   3. The names "The National Cancer Institute", "NCI" and "NGIT" must 
+ *      not be used to endorse or promote products derived from this software.
+ *   4. This license does not authorize the incorporation of this software
+ *      into any third party proprietary programs. This license does not 
+ *      authorize the recipient to use any trademarks owned by either NCI 
+ *      or NGIT 
+ *   5. THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED 
+ *      WARRANTIES, (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ *      OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE) ARE 
+ *      DISCLAIMED. IN NO EVENT SHALL THE NATIONAL CANCER INSTITUTE,
+ *      NGIT, OR THEIR AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ *      INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ *      BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ *      LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ *      CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ *      LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ *      ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ *      POSSIBILITY OF SUCH DAMAGE.
+ * <!-- LICENSE_TEXT_END -->
+ */
+
+/**
+ * @author EVS Team
+ * @version 1.0
+ *
+ * Note: This class is created based on Mayo's BuildTreForCode.java sample code
+ *
+ * Modification history
+ *     Initial modification kim.ong@ngc.com
+ *
+ */
 
 /**
  * Attempts to provide a tree, based on a focus code, that includes the
