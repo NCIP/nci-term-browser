8dcddd650372efa628d551a172eb3bfc8db1a232
David Yee <yeed>
2010-06-11 13:43:14 +0000

    Added warning message when MetadataUtils.getMetadataValue method returns   no value.  This will hopefully help isolate when specific metadata is not   loaded.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/trunk@1401 0604bb77-1110-461e-859e-28c50bf9b280

--- software/ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/MetadataUtils.java
+++ software/ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/MetadataUtils.java
@@ -330,8 +330,11 @@ public class MetadataUtils {
             return "";
         }
         int n = v.size();
-        if (n <= 0)
+        if (n <= 0) {
+            _logger.warn("WARNING: getMetadataValue(\"" + propertyName + "\"): returns no value.");
+            _logger.warn("  * Note: This metadata might not be loaded.");
             return "";
+        }
         if (v.size() == 1)
             return v.elementAt(0).toString();
 
