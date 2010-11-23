77df7f07e88b17a09f96da1a5a79dc318e4d6ee0
Will Garcia <garciawa2>
2010-11-23 01:45:31 +0000

    Remove concept from cart functionality.
    SVN-Revision: 1767

--- software/ncitbrowser/src/java/gov/nih/nci/evs/browser/bean/CartActionBean.java
+++ software/ncitbrowser/src/java/gov/nih/nci/evs/browser/bean/CartActionBean.java
@@ -19,6 +19,7 @@ public class CartActionBean {
 	
 	private String _codename = null;
 	private HashMap<String, Concept> _cart = null;
+	private String _backurl = null;
 
 	// Getters & Setters
 
@@ -42,9 +43,35 @@ public class CartActionBean {
 		if (_cart == null) return 0;
 		return _cart.size();
 	}
+
+	/**
+	 * Compute a back to url that is not the cart page
+	 * @return
+	 */
+	public String getBackurl() {
+        HttpServletRequest request =
+            (HttpServletRequest) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequest();		
+        String targetPage = request.getHeader("Referer"); 
+    	targetPage = (targetPage == null) ? request.getContextPath() : targetPage;		
+        if (!targetPage.contains("cart.jsf")) _backurl = targetPage; 
+        return _backurl;
+	}	
 	
+	/**
+	 * Return the concept collection 
+	 * @return
+	 */
 	public Collection<Concept> getConcepts() {
+		if (_cart == null) _init();
 		return _cart.values();
+	}
+	
+	/**
+	 * Initialize the cart container
+	 */
+	private void _init() {
+		if (_cart == null) _cart = new HashMap<String, Concept>();
 	}
 	
     /**
@@ -52,8 +79,7 @@ public class CartActionBean {
      * @return
      * @throws Exception
      */
-    public String addToCart() throws Exception {    	    	
-
+    public void addToCart() throws Exception {    	    	
     	String code = null;
     	String dictionary = null;  
     	String name = null;
@@ -70,20 +96,33 @@ public class CartActionBean {
         
         // Add concept to cart
         
-        if (_cart == null) _cart = new HashMap<String, Concept>();
+        if (_cart == null) _init();
         Concept item = new Concept();
         item.setCode(code); 
         item.setDictionary(dictionary);
         item.setName(name);
         
         if (!_cart.containsKey(code))
-        	_cart.put(code,item);
-        
-        System.out.println(toString());		// Debug
-
-        return null;
+        	_cart.put(code,item);        
     }
    
+    /**
+     * Remove concept(s) from the Cart
+     * @return
+     * @throws Exception
+     */
+    public void removeFromCart() {  
+        if (_cart != null && _cart.size() > 0) {
+            for (Iterator<Concept> i = getConcepts().iterator(); i.hasNext();) {
+            	Concept item = (Concept)i.next();	
+            	if (item.getSelected()) {
+            		if (_cart.containsKey(item.code))
+            			i.remove();
+            	}	
+            }        	
+        }    	
+    }
+    
     /**
      * Subclass to hold contents of the cart
      * @author garciawa2
@@ -92,11 +131,12 @@ public class CartActionBean {
     	private String code = null;
     	private String dictionary = null;
     	private String name = null;
+    	private boolean selected = false;
     	
     	// Getters & setters
     	
     	public String getCode() {
-    		return code;
+    		return this.code;
     	}
 
     	public void setCode(String code) {
@@ -104,7 +144,7 @@ public class CartActionBean {
     	}    	
     	
     	public String getDictionary() {
-    		return dictionary;
+    		return this.dictionary;
     	}
     	
     	public void setDictionary(String dictionary) {
@@ -112,12 +152,21 @@ public class CartActionBean {
     	}   
     	
     	public String getName() {
-    		return name;
+    		return this.name;
     	}
 
     	public void setName(String name) {
     		this.name = name;
-    	}     	
+    	}  
+
+    	public void setSelected(Boolean value) {
+    		this.selected = value;
+    	}
+    	
+    	public Boolean getSelected() {
+    		return this.selected;
+    	}
+ 	
     } // End of Concept
 
     //**
@@ -140,9 +189,10 @@ public class CartActionBean {
                 sb.append("\t      Code = " + item.code + "\n");
                 sb.append("\tDictionary = " + item.dictionary + "\n");   
                 sb.append("\t      Name = " + item.name + "\n");
+                sb.append("\t   Checked = " + item.selected + "\n");
             }        	
         } else {
-        	sb.append("Cart is empty.n");       	
+        	sb.append("Cart is empty.");       	
         }
 
         return sb.toString();
