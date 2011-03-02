810c5db8b49c385b22ce27adb9838ae5ffea1cec
David Yee <yeed>
2011-03-02 16:18:34 +0000

    GF29865: First pass at fixing mapping home page link since the display name   and version number (metadata values) are currently not unique.  This pertains   to the two "PDQ to NCIt Mapping: National Cancer Institure Thesaurus (201010)"   links in the Mapping start page.
    SVN-Revision: 2027

--- software/ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/OntologyInfo.java
+++ software/ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/OntologyInfo.java
@@ -4,33 +4,36 @@ import java.util.*;
 
 public class OntologyInfo {
     private String _displayName = "";
+    private String _codingScheme = "";
     private int _sortCategory = 0;
 
-    public OntologyInfo(String displayName, String sortCategory) {
-        if (sortCategory == null || sortCategory.trim().length() <= 0) {
-            init(displayName, 0);
-            return;
-        }
-
+    public OntologyInfo(String displayName, String codingScheme, String sortCategory) {
         try {
+            if (sortCategory == null || sortCategory.trim().length() <= 0)
+                throw new Exception("Sort Category not defined.");
             int category = Integer.parseInt(sortCategory);
-            init(displayName, category);
+            init(displayName, codingScheme, category);
         } catch (Exception e) {
-            init(displayName, 0);
+            init(displayName, codingScheme, 0);
         }
     }
 
-    public OntologyInfo(String displayName, int sortCategory) {
-        init(displayName, sortCategory);
+    public OntologyInfo(String displayName, String codingScheme, int sortCategory) {
+        init(displayName, codingScheme, sortCategory);
     }
 
-    private void init(String displayName, int sortCategory) {
+    private void init(String displayName, String codingScheme, int sortCategory) {
         _displayName = displayName;
+        _codingScheme = codingScheme;
         _sortCategory = sortCategory;
     }
 
     public String getDisplayName() {
         return _displayName;
+    }
+
+    public String getCodingScheme() {
+        return _codingScheme;
     }
 
     public int getSortCategory() {
