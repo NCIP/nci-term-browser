01d22e5158b7852433dc6fd795db3cb6625b7974
David Yee <yeed>
2011-12-13 15:02:45 +0000

    Undo commit.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/branches/2011-11-29.508.tree@2562 0604bb77-1110-461e-859e-28c50bf9b280

--- software/ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/DataUtils.java
+++ software/ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/DataUtils.java
@@ -131,7 +131,7 @@ public class DataUtils {
     private static HashMap _csnv2codingSchemeNameMap = null;
     private static HashMap _csnv2VersionMap = null;
 
-    private static boolean initializeValueSetHierarchy = false; //DYEE
+    private static boolean initializeValueSetHierarchy = true;
 
     // ==================================================================================
     // For customized query use
