66cb2351c041923eaf769c41e4d4bd1778fb9cd3
David Yee <yeed>
2012-02-21 17:39:15 +0000

    LEXEVS-471: Modified Scott's Bar.java value set test program.
    SVN-Revision: 2682

--- software/test/src/gov/nih/nci/evs/browser/test/valueset/Bar.java
+++ software/test/src/gov/nih/nci/evs/browser/test/valueset/Bar.java
@@ -19,38 +19,45 @@ public class Bar {
         String evsServiceUrl =
             NCItBrowserProperties
                 .getProperty(NCItBrowserProperties.EVS_SERVICE_URL);
-        System.out.println("EVS_SERVICE_URL: " + evsServiceUrl);
-        System.out.println("");
-        System.out.println(Utils.SEPARATOR);
+        debug("EVS_SERVICE_URL: " + evsServiceUrl);
+        debug("");
+        debug(Utils.SEPARATOR);
         new Bar().testGetValueSets();
     }
 
-    public void testGetValueSets(){
-        System.out.println("Before VSDS init");
-         //LexEVSValueSetDefinitionServices vsdServ = LexEVSServiceHolder.instance().getLexEVSAppService().getLexEVSValueSetDefinitionServices();
-         LexEVSValueSetDefinitionServices vsdServ = RemoteServerUtil.getLexEVSValueSetDefinitionServices();
-          CodedNodeSetImpl nodeSet = null;
-         Date start = new Date();       
-         try {
-             List<String> vsdDefURIs = vsdServ.listValueSetDefinitionURIs();
-             for(String s: vsdDefURIs){
-                 System.out.println(s);
-            Date vsStart = new Date();
-              ResolvedValueSetCodedNodeSet nodes = vsdServ.getValueSetDefinitionEntitiesForTerm("cell", MatchAlgorithms.LuceneQuery.name(), new URI(s), null, null);
-            Date vsStop = new Date();
-            long vsTime = vsStop.getTime() - vsStart.getTime();
-            System.out.println("Value Set resolve time: " + (vsTime < 1000? vsTime + " milliseconds": vsTime / 1000 + " seconds"));
-              if(nodes != null){
-           if(nodeSet != null){
-        //  nodeSet = (CodedNodeSetImpl) nodeSet.union(nodes.getCodedNodeSet());
-           }
-           else{
-        //     nodeSet = (CodedNodeSetImpl) nodes.getCodedNodeSet();
-           }
-              }
+    public void testGetValueSets() {
+        debug("Before VSDS init");
+        LexEVSValueSetDefinitionServices vsdServ =
+            RemoteServerUtil.getLexEVSValueSetDefinitionServices();
+        CodedNodeSetImpl nodeSet = null;
+        Date start = new Date();
+        try {
+            List<String> vsdDefURIs = vsdServ.listValueSetDefinitionURIs();
+            int i = 0;
+            for (String uri : vsdDefURIs) {
+                Date vsStart = new Date();
+                ResolvedValueSetCodedNodeSet nodes =
+                    vsdServ.getValueSetDefinitionEntitiesForTerm("cell",
+                        MatchAlgorithms.LuceneQuery.name(), new URI(uri), null,
+                        null);
+                Date vsStop = new Date();
+                long vsTime = vsStop.getTime() - vsStart.getTime();
+                String vsd_name = DataUtils.valueSetDefiniionURI2Name(uri);
+                debug(String.format("%5s", i++ + ")") 
+                    + " " + String.format("%-30s", uri)
+                    + " " + String.format("%-90s", vsd_name)
+                    + " Resolve time: " + formatTiming(vsTime));
+                if (nodes != null) {
+                    if (nodeSet != null) {
+                        // nodeSet = (CodedNodeSetImpl)
+                        // nodeSet.union(nodes.getCodedNodeSet());
+                    } else {
+                        // nodeSet = (CodedNodeSetImpl) nodes.getCodedNodeSet();
+                    }
+                }
 
-             }
-        //   nodeSet.resolve(null,null,null);
+            }
+            // nodeSet.resolve(null,null,null);
         } catch (LBException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -58,11 +65,25 @@ public class Bar {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-         Date stop = new Date();
-         long ta = start.getTime();
-         long tb = stop.getTime();
-         
-         long diff = tb - ta;
-         System.out.println("elapsed time: " + (diff < 1000? diff + " milliseconds": diff / 1000 + " seconds"));
+        Date stop = new Date();
+        long ta = start.getTime();
+        long tb = stop.getTime();
+
+        long diff = tb - ta;
+        debug("elapsed time: " + timing(diff));
+    }
+
+    private static void debug(String text) {
+        System.out.println(text);
+    }
+    
+    private static String formatTiming(long duration) {
+        if (duration < 1000)
+            return String.format("%5s", duration) + " msec";
+        else return String.format("%5s", duration/1000) + " sec ";
+    }
+    
+    private static String timing(long duration) {
+        return (duration < 1000 ? duration + " msecs" : duration / 1000 + " seconds");
     }
 }
