c517dc8e9eab66a060de11f88b7376a55153de5f
David Yee <yeed>
2012-02-23 13:52:49 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/tags/Archived@2748 0604bb77-1110-461e-859e-28c50bf9b280

---
+++ v2.0/2011-11-21.1-v2.0/software/test/src/gov/nih/nci/evs/browser/test/DataUtilsTest.java
@@ -0,0 +1,48 @@
+package gov.nih.nci.evs.browser.test;
+
+import org.LexGrid.LexBIG.DataModel.Collections.*;
+import org.LexGrid.LexBIG.DataModel.Core.*;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.*;
+import org.LexGrid.LexBIG.LexBIGService.*;
+import org.apache.log4j.*;
+
+import gov.nih.nci.evs.browser.test.utils.*;
+import gov.nih.nci.evs.browser.utils.*;
+
+public class DataUtilsTest {
+    private static Logger _logger = getLogger(); 
+
+    private static Logger getLogger() {
+        Logger.getLogger("gov.nih.nci.evs").setLevel(Level.DEBUG);
+        Class<DataUtilsTest> c = DataUtilsTest.class;
+        Logger.getLogger(c).setLevel(Level.DEBUG);
+        return Logger.getLogger(c);
+    }
+    
+    private static void debug(String text) {
+        //System.out.println(text);
+        _logger.debug(text);
+    }
+    
+    public static void getVocabularyVersionByTagTest() throws Exception {
+        LexBIGService lbSvc = RemoteServerUtil.createLexBIGService();
+        CodingSchemeRenderingList csrl = lbSvc.getSupportedCodingSchemes();
+        CodingSchemeRendering[] csrs = csrl.getCodingSchemeRendering();
+        for (int i = 0; i < csrs.length; i++) {
+            CodingSchemeRendering csr = csrs[i];
+            CodingSchemeSummary css = csr.getCodingSchemeSummary();
+            String formalName = css.getFormalName();
+            String version = css.getRepresentsVersion();
+            String prodVersion =
+                DataUtils.getVocabularyVersionByTag(formalName, "PRODUCTION");
+            debug(formalName);
+            debug("  * version:    " + version);
+            debug("  * production: " + prodVersion);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        args = SetupEnv.getInstance().parse(args);
+        DataUtilsTest.getVocabularyVersionByTagTest();
+    }
+}
---
+++ v2.0/2011-11-21.1-v2.0/software/test/src/gov/nih/nci/evs/browser/test/SearchUtilsTest.java
@@ -0,0 +1,62 @@
+package gov.nih.nci.evs.browser.test;
+
+import java.util.*;
+
+import org.LexGrid.LexBIG.DataModel.Core.*;
+import org.LexGrid.LexBIG.Utility.Iterators.*;
+import org.apache.log4j.*;
+
+import gov.nih.nci.evs.browser.test.utils.*;
+import gov.nih.nci.evs.browser.utils.*;
+
+public class SearchUtilsTest {
+    private static Logger _logger = getLogger(); 
+
+    private static Logger getLogger() {
+        Logger.getLogger("gov.nih.nci.evs").setLevel(Level.DEBUG);
+        Class<SearchUtilsTest> c = SearchUtilsTest.class;
+        Logger.getLogger(c).setLevel(Level.DEBUG);
+        return Logger.getLogger(c);
+    }
+    
+    public void searchByName(String matchText) throws Exception {
+        Vector<String> schemes = new Vector<String>();
+        Vector<String> versions = new Vector<String>();
+        schemes.add("NCI Thesaurus"); versions.add("10.11e");
+        schemes.add("NCI Thesaurus"); versions.add("10.12c");
+        schemes.add("NCI Thesaurus"); versions.add("11.01e");
+        String source = "ALL";
+        String matchAlgorithm = "exactMatch";
+        boolean ranking = true;
+        int maxToReturn = 100000;
+        
+        _logger.debug("------------------------------------------------------");
+        _logger.debug("Method: SearchUtils().searchByName");
+        _logger.debug("  * schemes: " + schemes);
+        _logger.debug("  * versions: " + versions);
+        _logger.debug("  * matchText: " + matchText);
+        _logger.debug("  * source: " + source);
+        _logger.debug("  * matchAlgorithm: " + matchAlgorithm);
+        _logger.debug("  * ranking: " + ranking);
+        _logger.debug("  * maxToReturn: " + maxToReturn);
+        ResolvedConceptReferencesIteratorWrapper wrapper =
+            new SearchUtils().searchByName(schemes, versions, matchText,
+                source, matchAlgorithm, ranking, maxToReturn);
+        ResolvedConceptReferencesIterator iterator = wrapper.getIterator();
+        if (iterator == null)
+            return;
+        while (iterator.hasNext()) {
+            ResolvedConceptReference rcr = iterator.next();
+            String code = rcr.getCode();
+            String desc = rcr.getEntityDescription().getContent();
+            _logger.debug(code + ": " + desc);
+        }
+        _logger.debug("");
+    }
+
+    public static void main(String[] args) throws Exception {
+        args = SetupEnv.getInstance().parse(args);
+        new SearchUtilsTest().searchByName("Zinc Finger Protein GLI3");
+        new SearchUtilsTest().searchByName("C17410");
+    }
+}
