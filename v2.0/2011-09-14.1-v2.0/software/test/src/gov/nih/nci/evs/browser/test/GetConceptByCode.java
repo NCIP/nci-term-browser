b7005c9153cb61e2f8efbb58693acdcc0e0e49e5
David Yee <yeed>
2012-02-23 13:52:35 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncit/tags/Archived@2746 0604bb77-1110-461e-859e-28c50bf9b280

---
+++ v2.0/2011-09-14.1-v2.0/software/test/src/gov/nih/nci/evs/browser/test/GetConceptByCode.java
@@ -0,0 +1,39 @@
+package gov.nih.nci.evs.browser.test;
+
+import org.LexGrid.concepts.Entity;
+
+import gov.nih.nci.evs.browser.test.utils.SetupEnv;
+import gov.nih.nci.evs.browser.utils.DataUtils;
+
+public class GetConceptByCode {
+    public void search(String codingScheme, String version, String code) {
+        System.out.println("-------------------------------------------------");
+        System.out.println("Retrieving: " + codingScheme + ", " + version + ", " + code);
+        String tag = null;
+        Entity entity =
+            DataUtils.getConceptByCode(codingScheme, version, tag, code);
+        String info = "";
+        if (entity != null)
+            info =
+                entity.getEntityDescription().getContent() + ":"
+                    + entity.getEntityCode();
+        else info = "[Could not find: " + code + "]";
+        System.out.println("Entity: " + info);
+    }
+
+    public void search() {
+//        search("NCI Thesaurus", "10.08e", "C12434");
+//        search("GO", "September2010", "GO:0000003");
+//        search("GO", "September2010", "GO:0000075");
+//        search("GO", "September2010", "GO:0005102");
+//        search("GO", "September2010", "GO:0006281");
+//        search("GO_to_NCIt_Mapping", "1.0", "GO:0005102");
+        search("NCI Thesaurus", "10.12c", "C12434");
+        search("NCI Thesaurus", "10.11e", "C12434");
+    }
+
+    public static void main(String[] args) {
+        args = SetupEnv.getInstance().parse(args);
+        new GetConceptByCode().search();
+    }
+}
