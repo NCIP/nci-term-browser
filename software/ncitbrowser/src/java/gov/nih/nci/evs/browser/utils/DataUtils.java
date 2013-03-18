4d6267ad0360c50be727935fd50bad77c70a02f8
Kim Ong <ongki@mail.nih.gov>
2013-03-18 13:51:58 +0000

    [#32679] Change textual CAPTCHA background to make it easier to read. [KLO, 031813]
    SVN-Revision: 3009

--- software/ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/DataUtils.java
+++ software/ncitbrowser/src/java/gov/nih/nci/evs/browser/utils/DataUtils.java
@@ -131,7 +131,7 @@ public class DataUtils {
     private static HashMap _csnv2codingSchemeNameMap = null;
     private static HashMap _csnv2VersionMap = null;
 
-    private static boolean initializeValueSetHierarchy = false;//true;
+    private static boolean initializeValueSetHierarchy = true;
     private static boolean valueSetHierarchyInitialized = false;
 
     private static boolean hasNoValueSet = false;
