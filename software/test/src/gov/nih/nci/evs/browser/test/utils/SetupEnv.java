9799a9db59638550cc2311a4804157c22b4c329f
Mahidhar Narra <narram@mail.nih.gov>
2013-04-12 14:26:04 -0400

    add headers to the branch

--- software/test/src/gov/nih/nci/evs/browser/test/utils/SetupEnv.java
+++ software/test/src/gov/nih/nci/evs/browser/test/utils/SetupEnv.java
@@ -1,3 +1,10 @@
+/*L
+ * Copyright Northrop Grumman Information Technology.
+ *
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/nci-term-browser/LICENSE.txt for details.
+ */
+
 package gov.nih.nci.evs.browser.test.utils;
 
 import gov.nih.nci.evs.browser.properties.*;
