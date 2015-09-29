c51fdbdce28322963a3bc49dffb21c0e41c245e1
kimong <kim.ong@ngc.com>
2015-09-29 16:27:20 -0400

    [NCITERM-670] Term Browser AppScan Performance

--- software/browser-util/src/gov/nih/nci/evs/browser/common/Constants.java
+++ software/browser-util/src/gov/nih/nci/evs/browser/common/Constants.java
@@ -198,7 +198,7 @@ public class Constants {
                                  "/IFRAME ", "IFRAME ", "INPUT TYPE", "IMG DYNSRC", "IMG LOWSRC", "ALERT(",
                                  "JAVASCRIPT:ALERT", "BGSOUND SRC", "BR SIZE", "LINK REL",
                                  "META HTTP-EQUIV", "STYLE=", "XSS:EXPRESSION", "XSS STYLE",
-                                 "DIV STYLE", "BASE HREF", "OBJECT TYPE", "XML ID", "SION(EVAL",
+                                 "DIV STYLE", "BASE HREF", "OBJECT TYPE", "XML ID", "SION(EVAL","(EVAL",
                                  "%2BALERT", "+ALERT", "%22ONMOUSEOVER", "\"ONMOUSEOVER",
                                  "SPAN DATASRC", "A HREF=", "$WHERE", "SLEEP(", "$QUERY", ";DECLARE", "VARCHAR(", "E-"};
 
