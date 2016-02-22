88fddb7af0136dc3a2c8b245036ac199d795b64c
kimong <kim.ong@ngc.com>
2016-02-22 12:47:29 -0500

    [NCITERM-701] UI test utility fails to detect broken OBO formatted ontology View-In-Hierarchy trees.

---
+++ software/junit-test/runGenerator.bat
@@ -0,0 +1,12 @@
+set OCP=%CLASSPATH%
+set lib1=..\ncitbrowser\lib
+set lib2=..\software\ncitbrowser\extlib
+set lib3=.
+
+set CLASSPATH=.
+set CLASSPATH=%CLASSPATH%;%lib1%\*
+set CLASSPATH=%CLASSPATH%;%lib2%\*
+set CLASSPATH=%CLASSPATH%;%lib3%\*
+
+C:\jdk1.7.0_05\bin\java -d64 -Xms512m -Xmx4g -classpath %CLASSPATH% gov.nih.nci.evs.testUtil.ui.UITestGeneratorRunner
+set CLASSPATH=%OCP%
